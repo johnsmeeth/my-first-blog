105ca10f0acc0aef82eb3c93c7c066d7cf1a46c7
johnsmeeth <johnsmeeth0210@gmail.com>
2015-11-14 17:00:30 +0700

    Add post detail views and modify urls to match with post detail

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # Match with empty string: for this match with http://localhost:8000/
     url(r'^$', views.post_list, name = 'post_list'),
+    url(r'^blog/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
