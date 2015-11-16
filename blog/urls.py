cba1bee9f56a05d35cfc95e53ddc258facd64d31
johnsmeeth <johnsmeeth0210@gmail.com>
2015-11-16 10:31:57 +0700

    add create new post, edit post and security only can do above job when authenticated

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     # Match with empty string: for this match with http://localhost:8000/
     url(r'^$', views.post_list, name = 'post_list'),
-    url(r'^blog/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
