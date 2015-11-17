6c3c92aae4dfe5aec911b9936f2a4fd138e8c294
johnsmeeth <johnsmeeth0210@gmail.com>
2015-11-17 14:06:34 +0700

    draft list and some action on post

--- blog/models.py
+++ blog/models.py
@@ -12,5 +12,9 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def unpublish(self):
+        self.published_date timezone.n
+        self.save()
+
     def __str__(self):
         return self.title
